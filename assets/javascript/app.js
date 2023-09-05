d6d03ef542cd6ad05885a86193249238016bc300
sakshi pareek <sakshipareek505@gmail.com>
2023-09-05 11:58:05 +0530

    feedback mistake

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,13 +26,14 @@ $(document).ready(function () {
 
 // -----------navbar------------
 
-let label = document.querySelector("#label")
-let navbox = document.querySelector(".navbox")
-label.addEventListener("click", function () {
+let label = document.querySelector("#label") 
+let navbox = document.querySelector(".navbox") 
+label.addEventListener("click" , function (){
     navbox.classList.toggle("left-0")
 })
 function showNav() {
     document.body.classList.toggle('overflow-hidden')
+    document.getElementById("label").classList.toggle("cross_icon");
 }
 
 // ----------overflow-sloder--------------
